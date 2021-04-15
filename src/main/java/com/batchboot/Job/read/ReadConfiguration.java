e253c303f702d83ec32740236b08d9e092b9ef65
DESKTOP-00M7QIT\kwang <kwanghojun@nate.com>
2021-04-16 01:24:01 +0900

    test

--- src/main/java/com/batchboot/Job/read/ReadConfiguration.java
+++ src/main/java/com/batchboot/Job/read/ReadConfiguration.java
@@ -1,15 +1,23 @@
 package com.batchboot.Job.read;
 
+import java.util.HashMap;
+import java.util.List;
+
+import javax.batch.api.chunk.ItemReader;
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.batch.MyBatisBatchItemWriter;
 import org.mybatis.spring.batch.MyBatisPagingItemReader;
+import org.mybatis.spring.batch.builder.MyBatisPagingItemReaderBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
@@ -32,23 +40,31 @@ public class ReadConfiguration {
 	
 
 	@Bean(name ="myBatisPagingItemReader")
+	@StepScope
     public MyBatisPagingItemReader<testLowDto> read(@Qualifier("sqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
     	System.out.println("{MyBatisPagingItemReader}..Start..!");
+    	/**
         MyBatisPagingItemReader<testLowDto> myBatisPagingItemReader = new MyBatisPagingItemReader<testLowDto>();
-        myBatisPagingItemReader.setQueryId("selectLowData");
         myBatisPagingItemReader.setSqlSessionFactory(sqlSessionFactory);
-        myBatisPagingItemReader.setPageSize(10);
-        try {
-        	System.out.println("sqlSessionFactory..["+sqlSessionFactory+"]");
-        	/**
-        	testLowDto testDto=myBatisPagingItemReader.read();
-        	if(testDto !=null) {
-        		System.out.println("Read..testLowDto..["+testDto.toString()+"]");
+        myBatisPagingItemReader.setQueryId("mybatis.testMapper.selectLowData");
+        myBatisPagingItemReader.setPageSize(10); 
+        
+        SqlSession sqlSession= sqlSessionFactory.openSession(true);
+        List<testLowDto> list=sqlSession.selectList("mybatis.testMapper.selectLowData",new testLowDto());
+        System.out.println("list size.."+list.size());
+        if(list !=null) {
+        	for(int i=0; i < list.size();i++) {
+        		testLowDto dto=list.get(i);
+        		//System.out.println("dto Values..["+dto.toString()+"]");
         	}
-        	**/
-        } catch(Exception e) {
-        	e.printStackTrace();
         }
+        **/
+    	MyBatisPagingItemReader<testLowDto> myBatisPagingItemReader= new MyBatisPagingItemReaderBuilder<testLowDto>()
+        		.sqlSessionFactory(sqlSessionFactory)
+        		.queryId("mybatis.testMapper.selectLowData")
+        		.parameterValues(new HashMap())
+        		.pageSize(10)
+        		.build();    	
         return myBatisPagingItemReader;
     }
 	    
